e1f7c7c5291e04268bd6ffa92cf7b14ebb65a30e
Sam, IRL <sambirdthenerd@gmail.com>
2020-04-05 21:39:21 -0500

    create function boilerplates

--- util/nubbank.js
+++ util/nubbank.js
@@ -7,10 +7,25 @@ const request = require('request');
 
 const apiLocation = 'https://bank.nub.international/v1/';
 
+// These are just boilerplate functions and will be implemented properly
+// when the NubBank payments API is written.
+
 /**
  * Checks whether or not the user has a NubBank account.
  * @param {String} username
  */
 export const userHasAccount = async (username) => {
+
+};
+
+export const getAccountID = async (username) => {
+
+};
+
+export const getMoney = async (accountID) => {
+
+};
+
+export const makePayment = async (fromAccountID, toAccountID, amount, message) => {
 
 };
