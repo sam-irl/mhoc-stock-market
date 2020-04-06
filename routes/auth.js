1e0d3497d6d7c8bdc135b9d05a8c2dcd0f644487
Sam, IRL <sambirdthenerd@gmail.com>
2020-04-05 22:37:44 -0500

    prep for babel es6

---
+++ routes/auth.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const passport = require('passport');
+const crypto = require('crypto');
+const router = express.Router();
+
+router.get('/reddit', (req, res, next) => {
+    req.session.state = crypto.randomBytes(32).toString('hex');
+    passport.authenticate('reddit', {
+        state: req.session.state
+    })(req, res, next);
+});
+
+router.get('/reddit/callback', (req, res, next) => {
+    if (req.query.state === req.session.state) {
+        passport.authenticate('reddit', {
+            successRedirect: '/auth/login/success',
+            failureRedirect: '/auth/login'
+        })(req, res, next);
+    } else {
+        const err = new Error('states did not match when handling reddit login');
+        err.code = 403;
+        next(err);
+    }
+});
+
+router.get('/login', (req, res) => res.render('login'));
+
+router.get('/login/success', (req, res) => req.redirect('/me/profile'));
+
+export default router;
